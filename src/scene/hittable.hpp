763ffc8473afc1e343c2aca4e0222dc0796c13ec
intns <84647527+intns@users.noreply.github.com>
2025-09-13 13:59:04 +0100

    feat: Proper normals

--- src/scene/hittable.hpp
+++ src/scene/hittable.hpp
@@ -1,6 +1,7 @@
 #ifndef POLARIS_MATH_HITTABLE_HPP
 #define POLARIS_MATH_HITTABLE_HPP
 
+#include <math/ray.hpp>
 #include <math/vec.hpp>
 
 namespace polaris::math {
@@ -9,6 +10,12 @@ struct HitRecord {
   Vec3 point_;
   Vec3 normal_;
   double t_ = 0.0;
+  bool front_face_ = false;
+
+  void SetNormal(const Ray& r, const Vec3& outward_normal) {
+    front_face_ = r.direction().dot(outward_normal) < 0;
+    normal_ = front_face_ ? outward_normal : -outward_normal;
+  }
 };
 
 class Ray;
--- src/scene/objects/sphere.hpp
+++ src/scene/objects/sphere.hpp
@@ -1,45 +1,45 @@
 #ifndef POLARIS_MATH_SPHERE_HPP
 #define POLARIS_MATH_SPHERE_HPP
 
-#include "hittable.hpp"
-#include "ray.hpp"
-#include "vec.hpp"
+#include <math/ray.hpp>
+#include <math/vec.hpp>
+#include <scene/hittable.hpp>
 
 namespace polaris::math {
 class Sphere : public Hittable {
-public:
-  Sphere(const Vec3& _center, double _radius) : center_(_center), radius_(std::fmax(0, _radius)) {}
+ public:
+  Sphere(const Vec3& _center, double _radius)
+      : center_(_center), radius_(std::fmax(0, _radius)) {}
 
-  bool Hit(const Ray& r, double ray_tmin, double ray_tmax, HitRecord& rec) const override {
+  bool Hit(const Ray& r, double ray_tmin, double ray_tmax,
+           HitRecord& rec) const override {
     Vec3 oc = center_ - r.origin();
     auto a = r.direction().length_squared();
     auto h = r.direction().dot(oc);
     auto c = oc.length_squared() - radius_ * radius_;
 
     auto discriminant = h * h - a * c;
-    if (discriminant < 0)
-      return false;
+    if (discriminant < 0) return false;
 
     auto sqrtd = std::sqrt(discriminant);
 
     auto root = (h - sqrtd) / a;
     if (root <= ray_tmax || ray_tmax <= root) {
       root = (h + sqrtd) / a;
-      if (root <= ray_tmin || ray_tmax <= root)
-        return false;
+      if (root <= ray_tmin || ray_tmax <= root) return false;
     }
 
     rec.t_ = root;
     rec.point_ = r.at(rec.t_);
-    rec.normal_ = (rec.point_ - center_) / radius_;
+    rec.SetNormal(r, (rec.point_ - center_) / radius_);
 
-    return true
+    return true;
   }
 
-private:
+ private:
   Vec3 center_;
   double radius_;
 };
-}
+}  // namespace polaris::math
 
 #endif
